8b5aa7b4dd973c6fcd0da974a6860380d4aeb933
GarudaFX <franfra10j@gmail.com>
2023-12-25 17:47:39 +0800

    improved design

--- src/pages/VehicleList.js
+++ src/pages/VehicleList.js
@@ -5,60 +5,67 @@ import styles from "../components/Vehicle.module.css";
 import Brandlist from "../components/Brandlist";
 
 function Home() {
-  const [vehicles, setVehicles] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [vehiclesPerPage] = useState(8);
+	const [vehicles, setVehicles] = useState([]);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [vehiclesPerPage] = useState(8);
 
-  const getVehicles = useCallback(async () => {
-    try {
-      let url = `http://localhost:8000/api/model?page=${currentPage}&limit=${vehiclesPerPage}`;
+	const getVehicles = useCallback(async () => {
+		try {
+			let url = `http://localhost:8000/api/car/model?page=${currentPage}&limit=${vehiclesPerPage}`;
 
-      const response = await axios.get(url);
-      const { models, currentPage: page } = response.data;
-      setVehicles(models);
-      setCurrentPage(page);
-    } catch (error) {
-      console.log(error);
-    }
-  }, [currentPage, vehiclesPerPage]);
+			const response = await axios.get(url);
+			const { models, currentPage: page } = response.data;
 
-  useEffect(() => {
-    getVehicles();
-  }, [getVehicles]);
+			setVehicles(models);
+			setCurrentPage(page);
+		} catch (error) {
+			console.log(error);
+		}
+	}, [currentPage, vehiclesPerPage]);
 
-  const nextPage = () => {
-    setCurrentPage((prevPage) => prevPage + 1);
-  };
+	useEffect(() => {
+		getVehicles();
+	}, [getVehicles]);
 
-  const prevPage = () => {
-    setCurrentPage((prevPage) => prevPage - 1);
-  };
+	const nextPage = () => {
+		setCurrentPage((prevPage) => prevPage + 1);
+	};
 
-  return (
-    <div className={styles["home-page"]}>
-      <h1>CAR LIST</h1>
-      <Brandlist />
-      <div className={styles["vehicle-grid"]}>
-        {vehicles.map((vehicle, index) => (
-          <Vehicle key={index} vehicle={vehicle} />
-        ))}
-      </div>
-      <div className={styles["pagination-container"]}>
-        <span>Page: {currentPage}</span>
-        <div className={styles["pagination-buttons"]}>
-          <button onClick={prevPage} disabled={currentPage === 1}>
-            Prev Page
-          </button>
-          <button
-            onClick={nextPage}
-            disabled={vehicles.length < vehiclesPerPage}
-          >
-            Next Page
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+	const prevPage = () => {
+		setCurrentPage((prevPage) => prevPage - 1);
+	};
+
+	return (
+		<div className={styles["home-page"]}>
+			<h1>DealerFlow CAR LIST</h1>
+			<Brandlist />
+			<div className={styles["vehicle-grid"]}>
+				{vehicles.map((vehicle, index) => (
+					<Vehicle
+						key={index}
+						vehicle={vehicle}
+					/>
+				))}
+			</div>
+			<div className={styles["pagination-container"]}>
+				<span>Page: {currentPage}</span>
+				<div className={styles["pagination-buttons"]}>
+					<button
+						className='btn btn-primary'
+						onClick={prevPage}
+						disabled={currentPage === 1}>
+						Prev Page
+					</button>
+					<button
+						onClick={nextPage}
+						className='btn btn-primary'
+						disabled={vehicles.length < vehiclesPerPage}>
+						Next Page
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Home;
--- src/pages/sales.js
+++ src/pages/sales.js
@@ -3,77 +3,77 @@ import axios from "axios";
 import DataTable from "react-data-table-component";
 
 function SalesLastThreeYears() {
-  const [salesLastThreeYears, setsalesLastThreeYears] = useState([]);
+	const [salesLastThreeYears, setsalesLastThreeYears] = useState([]);
 
-  const getSalesLastThreeYears = async () => {
-    try {
-      const response = await axios.get("http://localhost:8000/api/sales");
-      console.log(response.data);
-      setsalesLastThreeYears(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+	const getSalesLastThreeYears = async () => {
+		try {
+			const response = await axios.get("http://localhost:8000/api/car/sales");
+			console.log(response.data);
+			setsalesLastThreeYears(response.data);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  useEffect(() => {
-    getSalesLastThreeYears();
-  }, []);
+	useEffect(() => {
+		getSalesLastThreeYears();
+	}, []);
 
-  const columns = [
-    {
-      name: "Customer",
-      selector: "customer.customerName", // Replace with your data key
-      sortable: true,
-    },
-    {
-      name: "Dealer",
-      selector: "dealer.dealerName", // Replace with your data key
-      sortable: true,
-    },
-    {
-      name: "Brand",
-      selector: "brand.brandName", // Replace with your data key
-      sortable: true,
-    },
-    {
-      name: "Model",
-      selector: "vehicleModel.modelName", // Replace with your data key
-      sortable: true,
-    },
-    {
-      name: "Price",
-      selector: "vehiclePrice", // Replace with your data key
-      sortable: true,
-    },
-    {
-      name: "Date Created",
-      selector: "createdAt", // Replace with your data key
-      sortable: true,
-    },
-    // Add more columns as needed...
-  ];
+	const columns = [
+		{
+			name: "Customer",
+			selector: "customer.customerName", // Replace with your data key
+			sortable: true,
+		},
+		{
+			name: "Dealer",
+			selector: "dealer.dealerName", // Replace with your data key
+			sortable: true,
+		},
+		{
+			name: "Brand",
+			selector: "brand.brandName", // Replace with your data key
+			sortable: true,
+		},
+		{
+			name: "Model",
+			selector: "vehicleModel.modelName", // Replace with your data key
+			sortable: true,
+		},
+		{
+			name: "Price",
+			selector: "vehiclePrice", // Replace with your data key
+			sortable: true,
+		},
+		{
+			name: "Date Created",
+			selector: "createdAt", // Replace with your data key
+			sortable: true,
+		},
+		// Add more columns as needed...
+	];
 
-  const customStyles = {
-    headCells: {
-      style: {
-        backgroundColor: "#060b26", // Adjust this color to your desired dark background
-        color: "#fff", // Text color for column headers
-      },
-    },
-  };
+	const customStyles = {
+		headCells: {
+			style: {
+				backgroundColor: "#060b26", // Adjust this color to your desired dark background
+				color: "#fff", // Text color for column headers
+			},
+		},
+	};
 
-  return (
-    <div className="table-container">
-      <h1>SALES</h1>
-      <DataTable
-        columns={columns}
-        data={salesLastThreeYears}
-        pagination // Enable pagination if needed
-        customStyles={customStyles} // Apply the custom styles to the table
-        // Additional DataTable props can be added as needed
-      />
-    </div>
-  );
+	return (
+		<div className='table-container'>
+			<h1>SALES</h1>
+			<DataTable
+				columns={columns}
+				data={salesLastThreeYears}
+				pagination // Enable pagination if needed
+				customStyles={customStyles} // Apply the custom styles to the table
+				// Additional DataTable props can be added as needed
+			/>
+		</div>
+	);
 }
 
 export default SalesLastThreeYears;
